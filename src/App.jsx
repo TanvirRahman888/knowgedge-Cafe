b11370d9ae2f0e391af406ea94820ca020d98af6
Tanvir Module 1 <tanvirrahman8888@gmail.com>
2024-03-14 02:02:16 +0600

    Data Loaded

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import './App.css'
+import Blogs from './components/Blogs/Blogs'
+import Header from './components/Header/Header'
 
 function App() {
 
@@ -6,7 +8,8 @@ function App() {
   return (
     <>
       
-      <h1 className='text-3xl bg-slate-500 text-center p-7 m-7'>Knowledge Cafe</h1>
+      <Header></Header>
+      <Blogs></Blogs>
 
     </>
   )
