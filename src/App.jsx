598554092208f36be6a927d45796a77682ad1285
Tanvir Module 1 <tanvirrahman8888@gmail.com>
2024-03-14 01:08:03 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      
+      <h1 className='text-3xl bg-slate-500 text-center p-7 m-7'>Knowledge Cafe</h1>
+
+    </>
+  )
+}
+
+export default App
